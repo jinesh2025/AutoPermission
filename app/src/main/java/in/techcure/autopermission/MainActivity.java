93596bfe6b3f3c80089380f8065d41bb081463e6
Jinesh Soni <jinesh2025@gmail.com>
2016-09-22 12:34:12 +0530

    //1st rev

--- app/src/main/java/in/techcure/autopermission/MainActivity.java
+++ app/src/main/java/in/techcure/autopermission/MainActivity.java
@@ -40,20 +40,7 @@ public class MainActivity extends BaseActivity {
 	}
 
 	public void testContactPerms(){
-		Cursor phones = getContentResolver().query(
-				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
-				null, null);
-		while (phones.moveToNext()) {
-
-			String name = phones
-					.getString(phones
-							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-
-			String phoneNumber = phones
-					.getString(phones
-							.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-		}
+		Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
 		phones.close();
 
 		mToast("Reading Contact Done !!");
